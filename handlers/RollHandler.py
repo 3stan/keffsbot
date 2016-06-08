0fb3c8651d725503fb38b89d50fb69cdd98f472c
Tristan Jung <3stan.jung@gmail.com>
2016-06-07 21:24:45 -0700

    Adding float support for roll

--- handlers/RollHandler.py
+++ handlers/RollHandler.py
@@ -9,10 +9,16 @@ class RollHandler(HandlerBase):
     def handle(self, message):
         message_content = message.content
         tokenized_content = message_content.split()
-        if len(tokenized_content) > 1 and util.RepresentsInt(tokenized_content[1]):
-            if int(tokenized_content[1]) > 0:
-                yield from self.client.send_message(message.channel, '{} rolled {}'.format(message.author.name, random.randint(0, int(tokenized_content[1]))))
-            else:
-                yield from self.client.send_message(message.channel, '{} tried to fuck up the bot. Nice try.'.format(message.author.name))
+        if len(tokenized_content) > 1:
+            if util.RepresentsInt(tokenized_content[1]):
+                if int(tokenized_content[1]) > 0:
+                    yield from self.client.send_message(message.channel, '{} rolled {}'.format(message.author.name, random.randint(0, int(tokenized_content[1]))))
+                else:
+                    yield from self.client.send_message(message.channel, '{} tried to fuck up the bot. Nice try.'.format(message.author.name))
+            if util.RepresentsFloat(tokenized_content[1]):
+                if float(tokenized_content[1]) > 0:
+                    yield from self.client.send_message(message.channel, '{} rolled {}'.format(message.author.name, random.uniform(0, float(tokenized_content[1]))))
+                else:
+                    yield from self.client.send_message(message.channel, '{} tried to fuck up the bot. Nice try.'.format(message.author.name))
         else:
             yield from self.client.send_message(message.channel, '{} rolled {}'.format(message.author.name, random.randint(0, 100)))
--- util.py
+++ util.py
@@ -7,10 +7,17 @@ def RepresentsInt(s):
     except:
         return False
 
+def RepresentsFloat(s):
+    try:
+        float(s)
+        return True
+    except:
+        return False
+
 def unix_time(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
     delta = dt - epoch
     return delta.total_seconds()
 
 def normalize_text(text):
-	return text.lower()
+    return text.lower()
